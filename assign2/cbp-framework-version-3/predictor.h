dc831ab320455866a79854f2708cccf0f1e23dba
Abhimanyu <abhimanyu29.18@gmail.com>
2018-03-02 07:57:51 +0530

    perceptron commit 1

--- assign2/cbp-framework-version-3/predictor.h
+++ assign2/cbp-framework-version-3/predictor.h
@@ -5,6 +5,8 @@
 #ifndef PREDICTOR_H_SEEN
 #define PREDICTOR_H_SEEN
 
+#include <stdlib.h>
+#include <math.h>
 #include <cstddef>
 #include <inttypes.h>
 #include <vector>
@@ -17,29 +19,46 @@ class PREDICTOR
     typedef uint32_t address_t;
 
   private:
-    typedef uint32_t history_t;
+    typedef uint64_t history_t;
     typedef uint8_t counter_t;
 
-    static const int BHR_LENGTH = 15;
+    static const int BHR_LENGTH = 34;
+    static const int THRESHOLD = 79;
+    static const int WEIGHT_BITS = 7;
+    static const int ENTRY_SIZE = WEIGHT_BITS*BHR_LENGTH;
     static const history_t BHR_MSB = (history_t(1) << (BHR_LENGTH - 1));
-    static const std::size_t PHT_SIZE = (std::size_t(1) << BHR_LENGTH);
+    // static const std::size_t PHT_SIZE = (std::size_t(1) << BHR_LENGTH);
+    static const std::size_t PHT_SIZE = (std::size_t(64*1024/ENTRY_SIZE));
     static const std::size_t PHT_INDEX_MASK = (PHT_SIZE - 1);
-    static const counter_t PHT_INIT = /* weakly taken */ 2;
+    // static const counter_t PHT_INIT = /* weakly taken */ 2;
+    static const int MAX_ABS_WEIGHT = 64;
+    int DOT_PRODUCT;
+    
+    typedef struct pht_entry
+    {
+        int weight[BHR_LENGTH+1];
+    } pht_entry;
+    
+    static const pht_entry PHT_INIT;
 
     history_t bhr;                // 15 bits
-    std::vector<counter_t> pht;   // 64K bits
+    std::vector<pht_entry> pht;   // 64K bits 8K bytes
 
     void update_bhr(bool taken) { bhr >>= 1; if (taken) bhr |= BHR_MSB; }
+
     static std::size_t pht_index(address_t pc, history_t bhr) 
         { return (static_cast<std::size_t>(pc ^ bhr) & PHT_INDEX_MASK); }
-    static bool counter_msb(/* 2-bit counter */ counter_t cnt) { return (cnt >= 2); }
-    static counter_t counter_inc(/* 2-bit counter */ counter_t cnt)
-        { if (cnt != 3) ++cnt; return cnt; }
-    static counter_t counter_dec(/* 2-bit counter */ counter_t cnt)
-        { if (cnt != 0) --cnt; return cnt; }
 
   public:
-    PREDICTOR(void) : bhr(0), pht(PHT_SIZE, counter_t(PHT_INIT)) { }
+    // PREDICTOR(void) : bhr(0), pht(PHT_SIZE, counter_t(PHT_INIT)) { }
+    PREDICTOR(void) : bhr(0){
+        pht.reserve(PHT_SIZE);
+        for (uint i = 0; i < PHT_SIZE; ++i)
+        {
+            for(int j=0; j<BHR_LENGTH+1;j++)
+                pht[i].weight[j] = 0;
+        }
+    }
     // uses compiler generated copy constructor
     // uses compiler generated destructor
     // uses compiler generated assignment operator
@@ -55,8 +74,20 @@ class PREDICTOR
             if (/* conditional branch */ br->is_conditional) {
                 address_t pc = br->instruction_addr;
                 std::size_t index = pht_index(pc, bhr);
-                counter_t cnt = pht[index];
-                prediction = counter_msb(cnt);
+                int output = pht[index].weight[0];
+                for (int i = 0; i < BHR_LENGTH; ++i)
+                {
+                    int t = bhr>>(BHR_LENGTH-i-1);
+                    if(t==1)
+                        output += pht[index].weight[i+1];
+                    else 
+                        output -= pht[index].weight[i+1];
+                }
+                DOT_PRODUCT = output;
+                if(output>=0)prediction = true;
+                else prediction = false;
+                // counter_t cnt = pht[index];
+                // prediction = counter_msb(cnt);
             }
             return prediction;   // true for taken, false for not taken
         }
@@ -67,15 +98,45 @@ class PREDICTOR
     void update_predictor(const branch_record_c* br, const op_state_c* os, bool taken)
         {
             if (/* conditional branch */ br->is_conditional) {
+                // bool prediction = get_prediction(br, os);
                 address_t pc = br->instruction_addr;
                 std::size_t index = pht_index(pc, bhr);
-                counter_t cnt = pht[index];
-                if (taken)
-                    cnt = counter_inc(cnt);
-                else
-                    cnt = counter_dec(cnt);
-                pht[index] = cnt;
-                update_bhr(taken);
+                int output = pht[index].weight[0];
+                for (int i = 0; i < BHR_LENGTH; ++i)
+                {
+                    int t = bhr>>(BHR_LENGTH-i-1);
+                    if(t==1)
+                        output += pht[index].weight[i+1];
+                    else 
+                        output -= pht[index].weight[i+1];
+                }
+                
+                bool prediction;
+                if(output>=0)prediction = true;
+                else prediction = false;
+
+                if(taken != prediction || abs(output)<=THRESHOLD){
+                    address_t pc = br->instruction_addr;
+                    std::size_t index = pht_index(pc, bhr);
+                    for (int i = 0; i < BHR_LENGTH; ++i)
+                    {
+                        if(abs(pht[index].weight[i+1])==MAX_ABS_WEIGHT)continue;
+                        if(taken){
+                            int t = bhr>>(BHR_LENGTH-i-1);
+                            if(t==1)
+                                pht[index].weight[i+1] += 1;
+                            else
+                                pht[index].weight[i+1] -= 1;
+                        }else{
+                            int t = bhr>>(BHR_LENGTH-i-1);
+                            if(t==1)
+                                pht[index].weight[i+1] -= 1;
+                            else
+                                pht[index].weight[i+1] += 1;  
+                        }
+                    }
+                }
+            update_bhr(taken);
             }
         }
 };
