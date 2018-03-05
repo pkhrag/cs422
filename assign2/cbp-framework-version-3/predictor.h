2e5265d8e2e1b3e118c43f9f770f93c1480cf298
Prakhar Agarwal <pkhrag@iitk.ac.in>
2018-03-05 23:42:17 +0530

    final

--- assign2/cbp-framework-version-3/predictor.h
+++ assign2/cbp-framework-version-3/predictor.h
@@ -22,53 +22,43 @@ class PREDICTOR
   private:
     typedef uint32_t history_t;
     typedef int8_t counter_t;
-    typedef unsigned __int128 longhistory_t;
+    typedef unsigned __int128 uint128_t;
 
     static const int BHR_LENGTH = 64;  // 64 bits
-    static const int BHR_LENGTH_NN = 64;  // 64 bits
-    static const uint64_t BHR_MASK = (BHR_LENGTH_NN!=64)?((uint64_t(1) << BHR_LENGTH_NN) - 1):(-1);  // 64 bits
-    static const int PHIST_LENGTH = 16; // 16 bits
-    static const int PHT_LENGTH = 11;
+    static const int BHR_LENGTH_NN = 64;  // 64 bits for neural net
+    static const uint64_t BHR_MASK = (BHR_LENGTH_NN!=64)?((uint64_t(1) << BHR_LENGTH_NN) - 1):(-1); 
+    static const int PAHIS_LENGTH = 16; // 16 bits
+    static const int PHT_LENGTH = 11;   // 2^11 size pht table
     static const int PHT_COUNT = 8;    // 8 tables
-    static const longhistory_t BHR_MSB = (longhistory_t(1) << (BHR_LENGTH - 1));
-    static const history_t PHIST_MSB = (history_t(1) << (PHIST_LENGTH - 1));
+    static const std::size_t INPUT_LENGTH = BHR_LENGTH_NN + PAHIS_LENGTH + PHT_COUNT + 1 + 1; // bias and ghel output bit
     static const std::size_t PHT_SIZE = (std::size_t(1) << PHT_LENGTH);
-    static const counter_t PHT_INIT = 4;
-    static const counter_t TC_BITS = 5;  // 5 bits
-    static const std::size_t INPUT_LENGTH = BHR_LENGTH_NN + PHIST_LENGTH + PHT_COUNT + 1 + 1;
+    static const counter_t TC_LENGTH = 4;  // 4 bits
     static const uint32_t TIMES = 2;
-    double training_data_input[INPUT_LENGTH];
 
+    double training_data_input[INPUT_LENGTH];
     genann *ann;
     bool predict_nn;
-    longhistory_t bhr;
-    history_t phist;
-    counter_t tc;
-    uint32_t theta;
+    bool prediction = false;
+    float prediction_value = 4;
+    
+    uint128_t bhr;  // branch history register
+    history_t pahis;  // path history register
+    counter_t tc; // threshold counter
+    uint32_t theta; //threshold
     std::vector<counter_t> phts[PHT_COUNT];   // 64K bits
 
-    static address_t revBits(address_t n) {
-      uint32_t bitCount = sizeof(address_t)*8;
-      uint32_t reversed = 0, temp;
-      for(uint32_t i=0; i<bitCount; ++i) {
-        temp = n & (1 << i);
-        if (temp)
-          reversed |= (1 << ((bitCount-1)-i));
-      }
-      return reversed;
-    }
     void update_bhr(bool taken) { bhr <<= 1; if (taken) bhr |= 1; }
-    void update_phist(address_t pc) { phist <<= 1; phist |= pc & 1; }
-    static std::size_t pht_index(address_t pc, longhistory_t bhr, history_t phist, int pht) {
+    void update_pahis(address_t pc) { pahis <<= 1; pahis |= pc & 1; }
+    static std::size_t pht_index(address_t pc, uint128_t bhr, history_t pahis, int pht_num) {
       std::size_t PHT_MASK = (size_t(1) << PHT_LENGTH)-1;
-      if(pht == 0) {
+      if(pht_num == 0) {
         return static_cast<std::size_t>(pc & PHT_MASK);
       }
-      uint32_t bhrBitCount = (pht==0 ? 0 : (uint32_t(1) << (pht-1)));
-      uint32_t phistBitCount = std::min(uint32_t(16), bhrBitCount);
-      uint64_t append = phist & ((1 << phistBitCount) - 1);
-      append |= ((bhr & ((1 << bhrBitCount) - 1))  << phistBitCount);
-      append |= (longhistory_t(pc) << (bhrBitCount + phistBitCount));
+      uint32_t bhrBitCount = (pht_num==0 ? 0 : (uint32_t(1) << (pht_num-1)));
+      uint32_t pahisBitCount = std::min(uint32_t(16), bhrBitCount);
+      uint64_t append = pahis & ((1 << pahisBitCount) - 1);
+      append |= ((bhr & ((1 << bhrBitCount) - 1))  << pahisBitCount);
+      append |= (uint128_t(pc) << (bhrBitCount + pahisBitCount));
 
       std::size_t ret = append & PHT_MASK;
       append >>= PHT_LENGTH;
@@ -83,7 +73,7 @@ class PREDICTOR
     static counter_t counter_dec(counter_t cnt)
         { if (cnt != -8) --cnt; return cnt; }
     void tc_inc(){
-      if (tc != ((counter_t(1) << (TC_BITS-1)) - 1))
+      if (tc != ((counter_t(1) << (TC_LENGTH-1)) - 1))
         ++tc;
       else {
         tc= 0;
@@ -92,7 +82,7 @@ class PREDICTOR
       return;
     }
     void tc_dec(){
-      if (tc != (counter_t(1) << (TC_BITS-1)))
+      if (tc != (counter_t(1) << (TC_LENGTH-1)))
         --tc;
       else {
         tc= 0;
@@ -107,23 +97,23 @@ class PREDICTOR
             training_data_input[i] = double(masked_bhr & 1);
             masked_bhr >>= 1;
         }
-        uint32_t temp_phist = phist;
-        for (int i = 0; i < PHIST_LENGTH; ++i)
+        uint32_t temp_pahis = pahis;
+        for (int i = 0; i < PAHIS_LENGTH; ++i)
         {
-            training_data_input[BHR_LENGTH_NN + i] = double(temp_phist & 1);
-            temp_phist >>= 1;
+            training_data_input[BHR_LENGTH_NN + i] = double(temp_pahis & 1);
+            temp_pahis >>= 1;
         }
         for (int i = 0; i < PHT_COUNT; ++i)
         {
-            std::size_t index = pht_index(pc, bhr, phist, i);
+            std::size_t index = pht_index(pc, bhr, pahis, i);
             counter_t cnt = phts[i][index];//bias
-            training_data_input[i + BHR_LENGTH_NN + PHIST_LENGTH] = 2*(double(cnt) + 0.5);
+            training_data_input[i + BHR_LENGTH_NN + PAHIS_LENGTH] = 2*(double(cnt) + 0.5);
         }
         training_data_input[INPUT_LENGTH-1]= double(1);
     }
 
   public:
- PREDICTOR(void) : bhr(0), phist(0), theta(PHT_COUNT/2) {
+ PREDICTOR(void) : bhr(0), pahis(0), theta(PHT_COUNT/2) {
       std::vector<counter_t> init(PHT_SIZE, 4);
       for(int i=0; i<PHT_COUNT; ++i) {
         phts[i] = init;
@@ -141,10 +131,15 @@ class PREDICTOR
     // conditional branches.
     bool get_prediction(const branch_record_c* br, const op_state_c* os)
         {
-            bool prediction = false;
+            prediction = false;
             if (/* conditional branch */ br->is_conditional) {
-              float prediction_value = get_prediction_value(br, os);
               address_t pc = br->instruction_addr;
+              prediction_value = 4; // weekly taken bias
+              for(int i=0; i<PHT_COUNT; ++i){
+                std::size_t index = pht_index(pc, bhr, pahis, i);
+                counter_t cnt = phts[i][index];
+                prediction_value += cnt;
+              }
               prediction = (prediction_value >= 0 ? true : false);
               training_data_input[INPUT_LENGTH-2] = (double(prediction) - 0.5)*10;
               fill_input(pc);
@@ -155,16 +150,6 @@ class PREDICTOR
             return predict_nn;
         }
 
-    float get_prediction_value(const branch_record_c* br, const op_state_c* os) {
-      address_t pc = br->instruction_addr;
-      float prediction_value = PHT_COUNT/2.0;
-      for(int i=0; i<PHT_COUNT; ++i){
-        std::size_t index = pht_index(pc, bhr, phist, i);
-        counter_t cnt = phts[i][index];
-        prediction_value += cnt;
-      }
-      return prediction_value;
-    }
     // Update the predictor after a prediction has been made.  This should accept
     // the branch record (br) and architectural state (os), as well as a third
     // argument (taken) indicating whether or not the branch was taken.
@@ -172,11 +157,9 @@ class PREDICTOR
         {
                 address_t pc = br->instruction_addr;
           if (/* conditional branch */ br->is_conditional) {
-                bool prediction = get_prediction(br, os);
-                float prediction_value = get_prediction_value(br, os);
                 if(prediction != taken || abs(prediction_value) < theta) {
                   for(int i=0; i<PHT_COUNT; ++i){
-                    std::size_t index = pht_index(pc, bhr, phist, i);
+                    std::size_t index = pht_index(pc, bhr, pahis, i);
                     counter_t cnt = phts[i][index];
                     if (taken)
                       cnt = counter_inc(cnt);
@@ -197,10 +180,10 @@ class PREDICTOR
                     genann_train(ann, training_data_input, output, 0.0005);
                 }
                 update_bhr(taken);
-                update_phist(pc);
+                update_pahis(pc);
           } else if (br->is_return || br->is_call) {
             update_bhr(true);
-            update_phist(pc);
+            update_pahis(pc);
           }
         }
 };
